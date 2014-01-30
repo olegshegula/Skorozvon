1f4456b4c4d02a7a1be940cbcba976122c0b5861
oleg <oleg.shegula@gmail.com>
2014-01-30 20:22:37 +0200

    very last commit

--- src/com/skorozvon/pages/CreateContactPage.java
+++ src/com/skorozvon/pages/CreateContactPage.java
@@ -7,12 +7,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
-
+import org.openqa.selenium.support.PageFactory;
 
 
 /**
  * @author shcegula-os
- *
+ * 
  */
 public class CreateContactPage extends Page {
 
@@ -20,11 +20,11 @@ public class CreateContactPage extends Page {
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@CacheLookup
 	@FindBy(id = "name")
 	private WebElement name;
-	
+
 	@CacheLookup
 	@FindBy(id = "city")
 	private WebElement city;
@@ -32,25 +32,30 @@ public class CreateContactPage extends Page {
 	@CacheLookup
 	@FindBy(id = "post")
 	private WebElement post;
-	
+
 	@CacheLookup
 	@FindBy(id = "phone")
 	private WebElement phone;
-	
+
 	@CacheLookup
 	@FindBy(id = "email")
 	private WebElement email;
-	
+
 	@CacheLookup
 	@FindBy(id = "comment")
 	private WebElement comment;
-	
+
 	@CacheLookup
 	@FindBy(xpath = "//div[@class='popup popup__modal ui-draggable']")
 	private WebElement popupwindow;
-	
-	
-	/* (non-Javadoc)
+
+	@CacheLookup
+	@FindBy(xpath = "//button[text()='Сохранить']")
+	private WebElement savebutton;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.skorozvon.pages.Page#open()
 	 */
 	@Override
@@ -58,7 +63,7 @@ public class CreateContactPage extends Page {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public boolean isPageOpened() {
 		return popupwindow.isDisplayed();
 	}
@@ -67,9 +72,35 @@ public class CreateContactPage extends Page {
 		name.sendKeys(name_);
 		return this;
 	}
-	
+
 	public CreateContactPage enterCity(String city_) {
-		name.sendKeys(city_);
+		city.sendKeys(city_);
 		return this;
+	}
+
+	public CreateContactPage enterPost(String post_) {
+		post.sendKeys(post_);
+		return this;
+	}
+
+	public CreateContactPage enterPhone(String phone_) {
+		phone.sendKeys(phone_);
+		return this;
+	}
+
+	public CreateContactPage enterEmail(String email_) {
+		email.sendKeys(email_);
+		return this;
+	}
+
+	public CreateContactPage enterComment(String comment_) {
+		comment.sendKeys(comment_);
+		return this;
+	}
+
+	public AccountPage clickSave() {
+		savebutton.click();
+		return PageFactory.initElements(driver, AccountPage.class);
+
 	}
 }
